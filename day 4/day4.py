ffafcf0e2a5fda20fe6ae531036897a9692baa97
Marina <maarina.faria@gmail.com>
2022-12-07 12:22:54 -0300

    Day 4 - Camp Cleanup - Part Two

--- day 4/day4.py
+++ day 4/day4.py
@@ -1,4 +1,5 @@
-result = 0
+resultPartOne = 0
+resultPartTwo = 0
 
 with open('input.txt') as file: 
   for line in file:
@@ -10,11 +11,15 @@ with open('input.txt') as file:
       for x in element:
         pair_int.append(eval(x))
       if(len(pair_int) == 4):
+        # Part One
         if(pair_int[0] <= pair_int[2] and pair_int[1] >= pair_int[3]):
-          print('entrei no if 1 com o par:', pair_int)
-          result+=1
+          resultPartOne+=1
         elif(pair_int[0] >= pair_int[2] and pair_int[1] <= pair_int[3]):
-          print('entrei no if 2 com o par:', pair_int)
-          result+=1
+          resultPartOne+=1
+        # Part Two
+        if(pair_int[1] >= pair_int[2] and pair_int[0] <= pair_int[3]):
+          print('entrei no if com o par:', pair_int)
+          resultPartTwo+=1
 
-print(result) 
+print(resultPartOne) 
+print(resultPartTwo) 
